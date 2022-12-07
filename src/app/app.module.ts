0ad4383a4e4ce20623e6da1874c9f5b88c4dd416
Vijay kumar <70283128+VIJAYVINNY@users.noreply.github.com>
2022-12-07 15:44:04 +0530

    created new components trainers and departments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 import { OrdersModule } from './orders/orders.module';
 import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
 import { FormsModule } from '@angular/forms';
+import { TrainersComponent } from './trainers/trainers.component';
+import { DepartmentsComponent } from './departments/departments.component';
 @NgModule({
   declarations: [
     AppComponent,
     ListEmployeesComponent,
+    TrainersComponent,
+    DepartmentsComponent,
     
    
   ],
