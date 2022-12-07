dda99cb3e94eedf50e020afceff8c008fb94d365
Kahkeshan04 <kahkeshan2299@gmail.com>
2022-12-07 15:32:58 +0530

    New component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { OrdersModule } from './orders/orders.module';
 import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
 import { FormsModule } from '@angular/forms';
+import { NewCompComponent } from './new-comp/new-comp.component';
 @NgModule({
   declarations: [
     AppComponent,
     ListEmployeesComponent,
+    NewCompComponent,
     
    
   ],
