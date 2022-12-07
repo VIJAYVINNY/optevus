47e1624607a1d722652c4ed4ba33c91ecc7475c6
Vijay kumar <70283128+VIJAYVINNY@users.noreply.github.com>
2022-12-07 19:48:06 +0530

    littlebit of changes

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -10,22 +10,22 @@ export interface Employee {
     // isAvtive?: boolean;
     imageUrl: any;
     package:any;
-    Address:any;
-}
-
-export class Book {
-    id: number=0;
-    author_one: string="";
-    author_two: string="";
-    author_three: string="";
-    title: string="";
-    subtitle: string="";
-    publisher: string="";
-    year: Date=new Date();
-    city: string="";
-    edition: number=0;
-       volume: number=0;
-       pages: number=0;
-      ISBN: string="";
-    barcode: string="";
+        Address:any;
     }
+
+    export class Book {
+        id: number=0;
+        author_one: string="";
+        author_two: string="";
+        author_three: string="";
+        title: string="";
+        subtitle: string="";
+        publisher: string="";
+        year: Date=new Date();
+        city: string="";
+        edition: number=0;
+        volume: number=0;
+        pages: number=0;
+        ISBN: string="";
+        barcode: string="";
+        }
