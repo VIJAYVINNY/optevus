6ef6a41b85b647b3a386ec3df74f356fdcebbdbc
Vijay kumar <70283128+VIJAYVINNY@users.noreply.github.com>
2022-12-14 08:54:03 +0530

    modifications done

--- src/app/converttospace.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConverttospacePipe } from './converttospace.pipe';
-
-describe('ConverttospacePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ConverttospacePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
